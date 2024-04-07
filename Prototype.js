d4b247b5e8b57cc744c7abb7a73912c39858a1ad
ezxpro <ezxpro@duck.com>
2024-04-07 14:51:26 -0300

    Obter mapa de estoque de um intervalor B1:D8, onde B = Produtos e D= quantidade em estoque

---
+++ Prototype.js
@@ -0,0 +1,20 @@
+function obterMapaEstoque()
+{
+  const idPlanilha = "1J5twCP4L4Smy7FP4JbARD3qVB_mv8rnQ1fRN5y6LvaQ";
+  const planilha = SpreadsheetApp.openById(idPlanilha);
+  const pagina = planilha.getSheetByName("Stock");
+  var intervalo = pagina.getRange('B1:D8');
+  var dados = intervalo.getValues();
+  var mapaEstoque = new Map();
+  for (var i = 0; i < dados.length; i++)
+  {
+    var produto = dados[i][0];
+    var quantidade = dados[i][2];
+
+    mapaEstoque.set(produto, quantidade);
+  }
+  for (p of mapaEstoque)
+  {
+    Logger.log(p);
+  }
+}
